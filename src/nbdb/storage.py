ec795961b5afb5e464b5523342cfb5348390b4f1
PerchunPak <git@perchun.it>
2024-06-11 11:06:34 +0200

    Save storage to file

--- src/nbdb/storage.py
+++ src/nbdb/storage.py
@@ -1,17 +1,35 @@
+import json
 import typing as t
+from pathlib import Path
 
 import typing_extensions as te
 
 SERIALIZABLE_TYPE: te.TypeAlias = "str | int | dict | None"
+INTERNAL_DATA_TYPE: te.TypeAlias = t.Dict[str, SERIALIZABLE_TYPE]
 
 
 class Storage:
-    def __init__(self) -> None:
-        self._data: t.Dict[str, SERIALIZABLE_TYPE] = {}
+    def __init__(self, data: INTERNAL_DATA_TYPE, path: Path) -> None:
+        self._data: INTERNAL_DATA_TYPE = {}
+        self._path = path
 
     @classmethod
-    async def init(cls) -> te.Self:
-        return cls()
+    async def init(cls, path: Path) -> te.Self:
+        data = await cls._read(path)
+        return cls(data, path)
+
+    @staticmethod
+    async def _read(path: Path) -> INTERNAL_DATA_TYPE:
+        if not path.exists():
+            return {}
+
+        with path.open("r") as f:
+            return json.load(f)  # type: ignore[no-any-return]
+
+    async def _write(self) -> None:
+        # TODO: tempfile
+        with self._path.open("w") as f:
+            json.dump(self._data, f)
 
     async def set(self, key: str, value: SERIALIZABLE_TYPE) -> None:
         if value is None:
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -12,9 +12,15 @@ async def storage() -> Storage:
 
 @pytest.mark.parametrize("value", ("abc", 123123, {"hello": "world"}))
 @pytest.mark.parametrize("value2", ("abc", 123123, {"hello": "world"}))
-async def test_storage_in_memory(storage: Storage, value: t.Any, value2: t.Any) -> None:  # type: ignore[misc] # any
+async def test_simple_storage(storage: Storage, value: t.Any, value2: t.Any) -> None:  # type: ignore[misc] # any
     await storage.set("abc", value)
     assert await storage.get("abc") == value
 
     await storage.set("akefoekof", value2)
     assert await storage.get("akefoekof") == value2
+
+
+async def test_storage_in_file(storage: Storage) -> None:
+    await storage.set("abc", {"hello": "world"})
+    await storage._write()
+    assert await storage._read() == {"abc": {"hello": "world"}}
