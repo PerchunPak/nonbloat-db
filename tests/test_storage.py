04e9215b2f8b14ef8e7fcd2cb3393f21068d9040
PerchunPak <git@perchun.it>
2024-06-14 10:20:39 +0200

    Small fixes to tests

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,17 +1,22 @@
+from __future__ import annotations
+
 import typing as t
 from pathlib import Path
 
 import pytest
+import typing_extensions as te
 
 from nbdb.storage import Storage
 
-STORAGE_FACTORY_RETURN_TYPE: t.TypeAlias = t.Callable[[], t.Coroutine[Storage]]
+STORAGE_FACTORY_RETURN_TYPE: te.TypeAlias = t.Callable[[], t.Awaitable[Storage]]
 
 
 @pytest.fixture
-async def storage_factory(tmp_path_factory: t.Callable[[], Path]) -> STORAGE_FACTORY_RETURN_TYPE:
-    async def factory() -> Storage:
-        return await Storage.init(tmp_path_factory() / "database.json")
+async def storage_factory(tmp_path_factory: pytest.TempPathFactory) -> STORAGE_FACTORY_RETURN_TYPE:
+    async def factory(path: t.Optional[Path] = None) -> Storage:
+        if path is None:
+            path = tmp_path_factory.mktemp("data") / "database.json"
+        return await Storage.init(path)
 
     return factory
 
@@ -31,10 +36,13 @@ async def test_simple_storage(storage: Storage, value: t.Any, value2: t.Any) -> 
     assert await storage.get("akefoekof") == value2
 
 
-async def test_storage_in_file(storage: Storage) -> None:
-    await storage.set("abc", {"hello": "world"})
-    await storage._write()
-    assert await storage._read() == {"abc": {"hello": "world"}}
+async def test_storage_in_file(storage_factory: STORAGE_FACTORY_RETURN_TYPE) -> None:
+    storage1 = await storage_factory()
+    await storage1.set("abc", {"hello": "world"})
+    await storage1._write()
+
+    storage2 = await storage_factory(storage1._path)  # type: ignore[call-arg]
+    assert storage2._data == {"abc": {"hello": "world"}}
 
 
 async def test_failure_during_write(storage: Storage) -> None:
@@ -45,4 +53,4 @@ async def test_failure_during_write(storage: Storage) -> None:
     await storage._write()  # TODO: simulate write failure
 
     storage2 = await Storage.init(storage._path)
-    assert storage2.get("abc") == "abc"
+    assert await storage2.get("abc") == "abc"
